0ebde6324a6639114ac538be11b0a155829eeb89
SandonLiu <liushengxun888@gmail.com>
2022-06-15 23:43:43 +0900

    bat文件编写 20220615 by刘

---
+++ CopySNR7220.bat
@@ -0,0 +1,35 @@
+@echo off
+
+::Initial Setting
+SET batFile=%~n0
+SET object=%batFile:~-7,7%
+SET Path=C:\Users\liucx\eclipse-workspace
+SET pathFrom=%Path%\testBatch\From\
+SET pathTo=%Path%\testBatch\To\
+
+::Create Path for Backup
+SET ymd=%date:~0,4%%date:~5,2%%date:~8,2%
+SET hms=%time:~0,2%%time:~3,2%_%time:~6,2%%time:~9,2%
+SET bakFile=%ymd%_%hms%
+SET pathToBackUp=%pathTo%bak\%bakFile%
+IF NOT EXIST %pathToBackUp% (mkdir %pathToBackUp%)
+
+::Create FileName
+SET file_exeConfig=%object%.exe.config
+SET file_logicTxt=%object%Logic.txt
+
+@echo on
+
+::Backup All Files
+REM "-----------------------Backup PathTo Files Start-----------------------"
+COPY %pathTo%%file_exeConfig% %pathToBackUp%
+COPY %pathTo%%file_logicTxt% %pathToBackUp%
+REM "-----------------------Backup PathTo Files End------------------------"
+
+::Force to Copy All Files
+REM "-----------------------Copy Files Start-----------------------"
+COPY %pathFrom%%file_exeConfig% %pathTo%
+COPY %pathFrom%%file_logicTxt% %pathTo%
+REM "-----------------------Copy Files End------------------------"
+
+pause
